c227a6d4af321cc49014aaff8aa3493bdc3d48d8
Doug Wright <mrdougwright@gmail.com>
2014-10-26 18:55:53 -0700

    remove useless var

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,6 @@ class WelcomeController < ApplicationController
 
   def index
     @best_selling_products = Product.limit(5)
-    @other_products  ## search 2 or 3 categories (maybe based on the user)
     @product_types = ProductType.all
     if current_user && current_user.admin?
       redirect_to admin_merchandise_products_url
