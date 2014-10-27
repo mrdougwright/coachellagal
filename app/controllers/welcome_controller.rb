c43f650b34391e9775b64efe2bdad023f77f2c58
Doug Wright <mrdougwright@gmail.com>
2014-10-26 19:04:41 -0700

    remove admin redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,8 +5,5 @@ class WelcomeController < ApplicationController
   def index
     @best_selling_products = Product.limit(5)
     @product_types = ProductType.all
-    if current_user && current_user.admin?
-      redirect_to admin_merchandise_products_url
-    end
   end
 end
